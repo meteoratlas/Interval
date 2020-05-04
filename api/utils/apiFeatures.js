4c9bad74c4e326605489887401c61007e1a7ccd4
meteoratlas <kellyaccnts@gmail.com>
2020-05-03 21:09:58 -0600

    add utility functions

--- api/utils/apiFeatures.js
+++ api/utils/apiFeatures.js
@@ -5,44 +5,42 @@ class APIFeatures {
   }
   filter() {
     const queryObj = { ...this.queryString };
-    const excluded = ["page", "sort", "limit", "fields"];
-    excluded.forEach((i) => delete queryObj[i]);
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((e) => delete queryObj[e]);
 
     let queryStr = JSON.stringify(queryObj);
-    // get the equality comparison keys and convert them to mongoDB syntax ($ prefix)
-    // gte = greater than or equals
-    // gt = greater than
-    // lt = less than
-    // lte = less than or equals
-    // https://docs.mongodb.com/manual/reference/operator/query/
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
     this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
   sort() {
     if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(",").join(" ");
+      const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
-      this.query.sort("-datePosted");
+      // sort by date (starting with newest) if no sorting option
+      this.query = this.query.sort('-createdAt');
     }
     return this;
   }
   limitFields() {
     if (this.queryString.fields) {
-      const fields = this.queryString.sort.split(",").join(" ");
+      const fields = this.queryString.fields.split(',').join(' ');
       this.query = this.query.select(fields);
     } else {
-      this.query = this.query.select("-__v"); // exclude this field
+      // exclude mongoDB metadata by default
+      this.query.select('-__v');
     }
     return this;
   }
-  pagination() {
+  paginate() {
     const page = parseInt(this.queryString.page) || 1;
-    const limit = parseInt(this.queryString.limit) || 1;
-    const skip = page - 1 * limit;
+    const limit = parseInt(this.queryString.limit) || 100;
+    const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
+
     return this;
   }
 }
