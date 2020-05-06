fd5ad6022778e30feb9600d56089b378477dc46b
meteoratlas <kellyaccnts@gmail.com>
2020-05-05 22:08:57 -0600

    user routes and handling

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,35 +1,61 @@
-const User = require("../models/user");
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+const protocols = require("./protocols");
 
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is currently under construction."
-  });
-};
+exports.getAllUsers = protocols.getAll(User);
+exports.getUser = protocols.getOne(User);
 
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is currently under construction."
-  });
-};
+// do not update passwords with this patch route!
+exports.patchUser = protocols.updateOne(User);
+exports.deleteUser = protocols.deleteOne(User);
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is currently under construction."
-  });
+exports.getMe = (req, res, next) => {
+    req.params.id = req.user.id;
+    next();
 };
 
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is currently under construction."
-  });
+exports.updateCurrentUser = catchAsync(async (req, res, next) => {
+    // if user posts password, raise error
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(
+            new AppError(
+                "Please change your password in the provided route.",
+                400
+            )
+        );
+    }
+    // filter out attributes we don't want updated
+    // ie, only allow name and email for now
+    const filteredBody = filterObj(req.body, "name", "email");
+
+    // update user document
+    const user = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true,
+    });
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            user,
+        },
+    });
+    next();
+});
+
+exports.filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach((el) => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
 };
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is currently under construction."
-  });
-};
+
+exports.deleteCurrentUser = catchAsync(async (req, res, next) => {
+    await User.findByIdAndUpdate(req.user.id, { active: false });
+    res.status(204).json({
+        status: "success",
+        data: null,
+    });
+});
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,17 +1,30 @@
 const express = require("express");
-const controller = require("../controllers/userControllers");
+const authController = require("./../controllers/authController");
+const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(controller.getAllUsers)
-  .post(controller.createUser);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.post("/forgotpassword", authController.forgotPassword);
+router.patch("/resetpassword/:token", authController.resetPassword);
 
+// All following routes after this middleware require authentication
+router.use(authController.protect);
+
+router.get("/me", userController.getMe, userController.getUser);
+
+router.patch("/updatepassword", authController.updatePassword);
+router.patch("/updatecurrentuser", userController.updateCurrentUser);
+router.delete("/deletecurrentuser", userController.deleteCurrentUser);
+
+router.use(authController.restrictTo("admin"));
+
+router.route("/").get(userController.getAllUsers); //.post(createUser);
 router
-  .route("/:id")
-  .get(controller.getUser)
-  .patch(controller.updateUser)
-  .delete(controller.deleteUser);
+    .route("/:id")
+    .get(userController.getUser)
+    .patch(userController.patchUser)
+    .delete(userController.deleteUser);
 
 module.exports = router;
