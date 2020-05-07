ed6a5493c6fe2173434b8759be3b25db1029732c
meteoratlas <kellyaccnts@gmail.com>
2020-05-06 22:16:22 -0600

    add authController

---
+++ api/controllers/authController.js
@@ -0,0 +1,247 @@
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+const sendEmail = require("./../utils/email");
+const { promisify } = require("util");
+
+const signToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+    const {
+        name,
+        email,
+        password,
+        passwordConfirm,
+        passwordChangedAt,
+        role,
+    } = req.body;
+
+    const newUser = await User.create({
+        name,
+        email,
+        password,
+        passwordConfirm,
+        passwordChangedAt,
+        role,
+    });
+
+    const token = signToken(newUser._id);
+
+    createAndSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // check if email & password were submitted
+    if (!email || !password) {
+        return next(
+            new AppError("Please provide a valid email and password", 400)
+        );
+    }
+
+    // check if password is correct
+    // +password gets the password field, which is not included in default queries
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError("Incorrect email or password", 401));
+    }
+
+    createAndSendToken(user, 200, res);
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+    // get token if it exists
+    let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+        return next(
+            new AppError(
+                "You are not currently logged in - please log in to get access.",
+                401
+            )
+        );
+    }
+
+    // validate token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+        return next(
+            new AppError(
+                "The user who requested this token no longer exists.",
+                401
+            )
+        );
+    }
+
+    // check if user changed their password after JWT was issued
+    // iat == "issued at"
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(
+            new AppError(
+                "User recently changed password - please log in again.",
+                401
+            )
+        );
+    }
+
+    // grant access to protected route
+    req.user = currentUser;
+    next();
+});
+
+// roles is an array containing allowed user types.
+// We wrap the middleware in a closure to pass parameters to it (not normally possible)
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        // we have access to req.user because the previous middleware (protect) always runs before this one
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new AppError(
+                    "You do not have permission to perform this action.",
+                    403
+                )
+            );
+        }
+        next();
+    };
+};
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+    // get user based on posted email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user)
+        return next(new AppError("No user with that email was found.", 404));
+    // generate random token
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+
+    const resetURL = `${req.protocol}://${req.get(
+        "host"
+    )}/api/v1/users/resetpassword/${resetToken}`;
+
+    const message = `Forgot your password? Submit a patch request with your new password and passwordconfirm to ${resetURL}. If you didn't reset your password, please ignore this message (TEMP).`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: "Your password reset token",
+            message,
+        });
+
+        res.status(200).json({
+            status: "success",
+            message: "Token sent to email",
+        });
+    } catch (error) {
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({ validateBeforeSave: false });
+
+        return next(
+            new AppError(
+                "There was an issue sending the passord reset email.",
+                500
+            )
+        );
+    }
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+    // get user based on the token
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(req.params.token)
+        .digest("hex");
+
+    // find the user with the hashed token, and check if the token has expired by seeing if its date is greater than the current date
+    const user = await User.findOne({
+        passwordResetToken: hashedToken,
+        passwordResetExpires: {
+            $gte: Date.now(),
+        },
+    });
+
+    // if token not expired & user exists, set new password
+    if (!user) {
+        return next(new AppError("Token is invalid or has expired.", 400));
+    }
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save(); // use validator here
+
+    // update changedPasswordAt (handled in user schema)
+
+    // log user in, send JWT
+    createAndSendToken(user, 200, res);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+    // get user (use +password to add pass to document result)
+    const user = await User.findById(req.user.id).select("+password");
+    // (we don't use findbyIDAndUpdate for anything related to passwords, as it skips middleware and doesn't encrypt data)
+
+    if (!user) {
+        return next(new AppError("No user with that id.", 400));
+    }
+
+    // check if posted password correct
+    if (
+        !(await user.correctPassword(req.body.passwordCurrent, user.password))
+    ) {
+        return next(new AppError("The provided password is incorrect.", 401));
+    }
+
+    // update password
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+
+    // log user in with JWT
+    createAndSendToken(user, 201, res);
+});
+
+const createAndSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        //secure: true, // unneeded in dev
+        httpOnly: true,
+    };
+    if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+    res.cookie("jwt", token, cookieOptions);
+
+    // remove password from the output (doesn't delete password!)
+    user.password = undefined;
+
+    res.status(statusCode).json({
+        status: "success",
+        token,
+        data: {
+            user,
+        },
+    });
+};
