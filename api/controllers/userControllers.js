1dd4f416a5b6c3650632678a72c9b4b56f02188e
meteoratlas <kellyaccnts@gmail.com>
2020-05-18 21:58:22 -0600

    user controller updae

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,18 +1,68 @@
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
-const protocols = require("./protocols");
+const factory = require("./handlerFactory");
+const multer = require("multer");
+const sharp = require("sharp");
 
-exports.getAllUsers = protocols.getAll(User);
-exports.getUser = protocols.getOne(User);
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   },
+// });
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith("image")) {
+        cb(null, true);
+    } else {
+        cb(new AppError("Not an image; please upload an image.", 400), false);
+    }
+};
+
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+
+exports.uploadUserPhoto = upload.single("photo");
+// not necessary, use the sign up route instead
+//const createUser = factory.createOne(User);
+
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+    if (!req.file) return next();
+
+    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+    await sharp(req.file.buffer)
+        .resize(500, 500)
+        .toFormat("jpeg")
+        .jpeg({ quality: 80 })
+        .toFile(`public/img/users/${req.file.filename}`);
+
+    next();
+});
+
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
 
 // do not update passwords with this patch route!
-exports.patchUser = protocols.updateOne(User);
-exports.deleteUser = protocols.deleteOne(User);
+exports.patchUser = factory.updateOne(User);
+exports.deleteUser = factory.deleteOne(User);
 
 exports.getMe = (req, res, next) => {
     req.params.id = req.user.id;
     next();
+};
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach((el) => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
 };
 
 exports.updateCurrentUser = catchAsync(async (req, res, next) => {
@@ -27,7 +77,13 @@ exports.updateCurrentUser = catchAsync(async (req, res, next) => {
     }
     // filter out attributes we don't want updated
     // ie, only allow name and email for now
+    // const filteredBody = filterObj(req.body, 'name', 'email');
     const filteredBody = filterObj(req.body, "name", "email");
+
+    if (req.file) {
+        filteredBody.photo = req.file.filename;
+        console.log(req.file.filename);
+    }
 
     // update user document
     const user = await User.findByIdAndUpdate(req.user.id, filteredBody, {
@@ -41,16 +97,7 @@ exports.updateCurrentUser = catchAsync(async (req, res, next) => {
             user,
         },
     });
-    next();
 });
-
-exports.filterObj = (obj, ...allowedFields) => {
-    const newObj = {};
-    Object.keys(obj).forEach((el) => {
-        if (allowedFields.includes(el)) newObj[el] = obj[el];
-    });
-    return newObj;
-};
 
 exports.deleteCurrentUser = catchAsync(async (req, res, next) => {
     await User.findByIdAndUpdate(req.user.id, { active: false });
