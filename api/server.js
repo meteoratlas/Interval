8ceed8051a5aa15b13fc2a6249abecb59d0f3227
meteoratlas <kellyaccnts@gmail.com>
2020-06-10 21:52:37 -0600

    server exceptions, db connection

--- api/server.js
+++ api/server.js
@@ -1,4 +1,37 @@
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 const app = require("./app");
 
+// global exception safety net
+process.on("unhandledRejection", (error) => {
+    console.warn("Unhandled Rejection - shutting down.");
+    server.close(() => {
+        // 1 == uncaught exception
+        process.exit(1);
+    });
+});
+
+if (process.env.NODE_ENV !== "development") {
+    process.on("uncaughtException", (error) => {
+        console.warn("Unhandled Exception - shutting down.");
+        server.close(() => {
+            // 1 == uncaught exception
+            process.exit(1);
+        });
+    });
+}
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.MONGODD_PASS);
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connection successful.");
+    });
+
 const port = 3001;
-app.listen(port);
+const server = app.listen(port);
